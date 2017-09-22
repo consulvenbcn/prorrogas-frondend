9e9086228ada701f91bc18c2ac0052bd3d5ab340
Nestor Reverol <nestor.reverol092@gmail.com>
2017-09-22 22:13:25 +0200

    topBar Component añadido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './items/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
