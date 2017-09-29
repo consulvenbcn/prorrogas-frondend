b570dd7d8980c973f0006b68623e48f186c61bfc
Nestor Reverol <nestor.reverol092@gmail.com>
2017-09-29 12:09:23 +0200

    add loader remove add Login Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 /*COMPONENTS*/
 import React, { Component } from 'react';
+import { Dimmer, Loader } from 'semantic-ui-react'
 /*APP-COMPONENTS*/
-import Topbar from './components/Topbar/Topbar';
-import Maincontent from './components/Maincontent/Maincontent';
+
+import Login from './util/Login/Login';
 
 class App extends Component {
+  state = {}
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({ active: false });
+    }, 2000);
+
+  }
+  componentWillMount(){
+    this.setState({ active: true });
+  }
   render() {
+    const { active } = this.state
     return (
-      <div>
-        <Topbar />
-        <Maincontent />
-      </div>
+      <Dimmer.Dimmable dimmed={active}>
+        <Dimmer active={active}>
+          <Loader size='massive'>Cargando...</Loader>
+        </Dimmer>
+        <Login />
+      </Dimmer.Dimmable>
     );
   }
 }
-
 export default App;
